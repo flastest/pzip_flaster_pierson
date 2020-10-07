ef63b8ff3c58eac0e1635f79b9fab3a2c7c41022
Talib Pierson <piersont@reed.edu>
2020-10-07 16:13:33 -0700

    reformat pzip.c
    Signed-off-by: Talib Pierson <piersont@reed.edu>

--- concurrency-pzip/pzip.c
+++ concurrency-pzip/pzip.c
@@ -151,9 +151,9 @@ int main(int argc, char *argv[]) {
         args._buffer = (buffer + (pid * size_of_each_threads_work));
         args._size = size_of_each_threads_work;
 
-        //simple way to make zip as compressed as possible, check here for
+        // simple way to make zip as compressed as possible, check here for
         // first/last thing in threads buffers being the same.
-        pthread_create(&threads[pid], NULL, zip_thread, (void *)&args);
+        pthread_create(&threads[pid], NULL, zip_thread, (void *) &args);
     }
 
     // join threads here
@@ -162,7 +162,7 @@ int main(int argc, char *argv[]) {
     }
 
     // Run-length-encode the buffer to stdout
-    //zip(buffer, size);
+    // zip(buffer, size);
 
     return EXIT_SUCCESS;
 }
