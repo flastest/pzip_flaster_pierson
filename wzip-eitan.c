5423fcac72adf176a85c5b479dd1f597c7c32474
Talib Pierson <47838810+TalibPierson@users.noreply.github.com>
2020-10-07 09:28:12 -0700

    Update wzip-eitan.c

--- wzip-eitan.c
+++ wzip-eitan.c
@@ -5,40 +5,46 @@
  */
 
 #include <errno.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdint.h>
 #include <string.h>
 #include <unistd.h>
 
-//////////////////////////////////////////////////////////////////////////////
-// Given an input file pointer, read and compress the contents of the file to
-// stdout.
+/**
+ * Read and compress the contents of a file to stdout.
+ * @param fp input file pointer
+ */
 void zip(FILE *fp) {
-    int c, last = fgetc(fp);
+    int curr, last = fgetc(fp);
     uint32_t count;
 
     while (last != EOF) {
-        for (count = 1; (c = fgetc(fp)) == last; count++)  // Count repeats of last
-            ;
+        // Count repeats of last
+        for (count = 1; (curr = fgetc(fp)) == last; count++) {
+        }
 
         if (fwrite(&count, sizeof(count), 1, stdout) < 1) {
             perror("Can't write to stdout");
             exit(1);
         }
         printf("%c", last);
-        last = c;
+        last = curr;
     }
 
     if (!feof(fp)) {
         perror("Can't read from file");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 }
 
-//////////////////////////////////////////////////////////////////////////////
-// Read all files listed in the argument list after a search term,
-// and output matching lines to standard output.
+/**
+ * Read all files listed in the argument list after a search term,
+ * and output matching lines to standard output.
+ * @param argc argument count
+ * @param argv arguments
+ * @return EXIT_SUCCESS iff successful
+ */
 int main(int argc, char **argv) {
     if (argc <= 1) {
         fprintf(stderr, "%s: searchterm file ...\n", argv[0]);
@@ -47,13 +53,13 @@ int main(int argc, char **argv) {
 
     for (int i = 1; i < argc; i++) {
         FILE *in = fopen(argv[i], "r");
-        if (!in) {
+        if (in == NULL) {
             perror("wunzip: cannot open file");
-            exit(1);
+            exit(EXIT_FAILURE);
         }
         zip(in);
         fclose(in);
     }
 
-    return 0;
+    return EXIT_SUCCESS;
 }
